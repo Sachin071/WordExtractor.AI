38b9b0f57f0dc9fe51824caa116a2d2f787a49bf
Shashankcognic <shashank.patel@cognicsys.com>
2023-10-10 12:34:19 +0530

    sss

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,83 @@ import React, { useEffect, useState } from "react";
 import FileUploadButton from "./FileUploadButton";
 import mammoth from "mammoth";
 import Navbar from "./components/Navbar";
+import Skeleton from "react-loading-skeleton";
 // import DynamicDiagnosisTable from "./components/Response";
 // import pdf from "pdf-parse";
 
 function App() {
   const [selectedFile, setSelectedFile] = useState(null);
-
   const [resultData, setResultData] = useState([]);
+
+  var lines = {};
+
+  const PostFetchData = async (filteredText) => {
+    // FilteredData();
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append(
+      "Authorization",
+      "Bearer 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"
+    );
+
+    // console.log("daaa", extractData);
+
+    var raw = JSON.stringify({
+      model: "gpt-4",
+      messages: [
+        {
+          role: "user",
+          content:
+            filteredText +
+            "Prompt: From a medical document with sections like HPI, ASSESSMENT, PATIENT ACTIVE PROBLEM LIST, PHYSICAL EXAM, PAST HISTORY, PROBLEM LIST, etc., extract diagnosis details including the diagnosis name, ICD-10 code, treatment, and visit date. Present this information in a table format.",
+        },
+      ],
+    });
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    await fetch("https://api.openai.com/v1/chat/completions", requestOptions)
+      .then((response) => response.json())
+      .then(async (result) => {
+        console.log(result.choices[0].message.content);
+        const dataString = result.choices[0].message.content;
+        // Parse the data string into an array of objects
+        const lines1 = dataString.split("\n");
+        var tableData = [];
+
+        // Loop through lines, skipping the header
+        for (var i = 2; i < lines1.length; i++) {
+          // Split each line by the delimiter (|) and remove any leading/trailing spaces
+          var columns = lines1[i].split("|").map(function (column) {
+            return column.trim();
+          });
+
+          // Create an object with the specified headers
+          var entry = {
+            "Visit Date": columns[1],
+            "Diagnosis Name": columns[2],
+            "ICD-10 Code": columns[3],
+            Treatment: columns[4],
+          };
+
+          // Add the entry to the tableData array
+          tableData.push(entry);
+        }
+
+        console.log("parseData --", tableData);
+
+        await setResultData(tableData);
+
+        console.log("resultData --", resultData);
+      })
+      .catch((error) => console.log("error", error));
+  };
 
   const extractDocxData = async (docxFile) => {
     const reader = new FileReader();
@@ -20,10 +90,9 @@ function App() {
       let text = result.value;
       console.log(text);
 
-      // Split the text into lines
-      const lines = await text.split("\n");
+      // // Split the text into lines
+      // lines = await text.split("\n");
 
-      // Filter out lines that don't contain the unwanted sections
       const filteredLines = await lines.filter((line) => {
         return !(
           line.includes("Acc No.") ||
@@ -41,71 +110,7 @@ function App() {
       // Output the result
       console.log("filtred data -- ", filteredText);
 
-      // await setExtractData(filteredText);
-
-      var myHeaders = new Headers();
-      myHeaders.append("Content-Type", "application/json");
-      myHeaders.append(
-        "Authorization",
-        "Bearer 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"
-      );
-
-      // console.log("daaa", extractData);
-
-      var raw = JSON.stringify({
-        model: "gpt-4",
-        messages: [
-          {
-            role: "user",
-            content:
-              filteredText +
-              "Prompt: From a medical document with sections like HPI, ASSESSMENT, PATIENT ACTIVE PROBLEM LIST, PHYSICAL EXAM, PAST HISTORY, PROBLEM LIST, etc., extract diagnosis details including the diagnosis name, ICD-10 code, treatment, and visit date. Present this information in a table format.",
-          },
-        ],
-      });
-
-      var requestOptions = {
-        method: "POST",
-        headers: myHeaders,
-        body: raw,
-        redirect: "follow",
-      };
-
-      await fetch("https://api.openai.com/v1/chat/completions", requestOptions)
-        .then((response) => response.json())
-        .then(async (result) => {
-          console.log(result.choices[0].message.content);
-          const dataString = result.choices[0].message.content;
-          // Parse the data string into an array of objects
-          const lines = dataString.split("\n");
-          var tableData = [];
-
-          // Loop through lines, skipping the header
-          for (var i = 2; i < lines.length; i++) {
-            // Split each line by the delimiter (|) and remove any leading/trailing spaces
-            var columns = lines[i].split("|").map(function (column) {
-              return column.trim();
-            });
-
-            // Create an object with the specified headers
-            var entry = {
-              "Visit Date": columns[1],
-              "Diagnosis Name": columns[2],
-              "ICD-10 Code": columns[3],
-              Treatment: columns[4],
-            };
-
-            // Add the entry to the tableData array
-            tableData.push(entry);
-          }
-
-          console.log("parseData --", tableData);
-
-          await setResultData(tableData);
-
-          console.log("resultData --", resultData);
-        })
-        .catch((error) => console.log("error", error));
+      PostFetchData(filteredText);
     };
 
     reader.readAsArrayBuffer(docxFile);
@@ -126,21 +131,66 @@ function App() {
     }
   };
 
+  // useEffect(() => {
+  //   if (selectedFile) {
+  //     PostFetchData();
+  //   }
+  // }, [selectedFile]);
+
   return (
     <div className="App">
       <Navbar />
       <header className="App-header">
         <FileUploadButton onFileSelect={handleFileSelect} />
         {selectedFile && <p>Selected File: {selectedFile.name}</p>}
-        {/* {resultData} */}
-        {/* <DynamicDiagnosisTable /> */}
-        <div
-          style={{
-            // margin : "0px "
-            padding: "15px 10px",
-          }}
-        >
-          <h2 style={{ marginBottom: "20px" }}>Diagnosis Details</h2>
+
+        {resultData.length === 0 ? (
+          <table style={{ width: "100%", borderCollapse: "collapse" }}>
+            <thead>
+              <tr>
+                <th style={{ borderBottom: "2px solid #ddd", padding: "8px" }}>
+                  <Skeleton height={20} />
+                </th>
+                <th style={{ borderBottom: "2px solid #ddd", padding: "8px" }}>
+                  <Skeleton height={20} />
+                </th>
+                <th style={{ borderBottom: "2px solid #ddd", padding: "8px" }}>
+                  <Skeleton height={20} />
+                </th>
+                <th style={{ borderBottom: "2px solid #ddd", padding: "8px" }}>
+                  <Skeleton height={20} />
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {[1, 2, 3].map((index) => (
+                <tr key={index}>
+                  <td
+                    style={{ borderBottom: "1px solid #ddd", padding: "8px" }}
+                  >
+                    <Skeleton height={20} />
+                  </td>
+                  <td
+                    style={{ borderBottom: "1px solid #ddd", padding: "8px" }}
+                  >
+                    <Skeleton height={20} />
+                  </td>
+                  <td
+                    style={{ borderBottom: "1px solid #ddd", padding: "8px" }}
+                  >
+                    <Skeleton height={20} />
+                  </td>
+                  <td
+                    style={{ borderBottom: "1px solid #ddd", padding: "8px" }}
+                  >
+                    <Skeleton height={20} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          // Render the actual data when loading is false
           <table
             style={{
               border: "2px solid black",
@@ -149,43 +199,14 @@ function App() {
               textAlign: "left",
             }}
           >
-            <thead>
-              <tr>
-                <th style={{ border: "2px solid black", padding: "8px" }}>
-                  Visit Date
-                </th>
-                <th style={{ border: "2px solid black", padding: "8px" }}>
-                  Diagnosis Name
-                </th>
-                <th style={{ border: "2px solid black", padding: "8px" }}>
-                  ICD-10 Code
-                </th>
-                <th style={{ border: "2px solid black", padding: "8px" }}>
-                  Treatment
-                </th>
-              </tr>
-            </thead>
+            {/* ... (your table headers) */}
             <tbody>
-              {resultData &&
-                resultData.map((entry, index) => (
-                  <tr key={index}>
-                    <td style={{ border: "2px solid black", padding: "8px" }}>
-                      {entry["Visit Date"]}
-                    </td>
-                    <td style={{ border: "2px solid black", padding: "8px" }}>
-                      {entry["Diagnosis Name"]}
-                    </td>
-                    <td style={{ border: "2px solid black", padding: "8px" }}>
-                      {entry["ICD-10 Code"]}
-                    </td>
-                    <td style={{ border: "2px solid black", padding: "8px" }}>
-                      {entry["Treatment"]}
-                    </td>
-                  </tr>
-                ))}
+              {resultData.map((entry, index) => (
+                <tr key={index}>{/* ... (your table data cells) */}</tr>
+              ))}
             </tbody>
           </table>
-        </div>
+        )}
       </header>
     </div>
   );
